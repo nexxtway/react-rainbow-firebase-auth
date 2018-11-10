47418a555237935f681e9683eb90de22af1fd96e
ayanes <alejandro.yanes94@gmail.com>
2018-11-10 02:12:11 -0500

    feat: implement auth view

--- src/components/pages/auth/index.js
+++ src/components/pages/auth/index.js
@@ -1,46 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-rainbow-components/components/Button';
+import classnames from 'classnames';
+import AvatarMenu from 'react-rainbow-components/components/AvatarMenu';
+import Avatar from 'react-rainbow-components/components/Avatar';
+import MenuDivider from 'react-rainbow-components/components/MenuDivider';
+import MenuItem from 'react-rainbow-components/components/MenuItem';
+import Select from 'react-rainbow-components/components/Select';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
     FormattedMessage,
-    injectIntl,
-    intlShape,
-    defineMessages,
 } from 'react-intl';
 import { logoutUser } from '../../../redux/actions/authentication';
+import PowerIcon from './powerIcon';
+import PencilIcon from './pencilIcon';
+import './styles.css';
+
+const languageOptions = [
+    {
+        value: 'en',
+        label: 'English',
+    },
+    {
+        value: 'es',
+        label: 'Español',
+    },
+];
 
 function AuthApp(props) {
     const {
         className,
         style,
         logoutUser,
-        user,
-        intl,
     } = props;
 
-    const translations = defineMessages({
-        logoutButton: {
-            id: 'authenticated.exp.logout',
-            defaultValue: 'Logout',
-        },
-    });
-
-    const userName = {
-        name: user.getUsername ? user.getUsername() : user.name,
-    };
-
-    console.log(userName, user);
+    const getContainerClassNames = () => classnames('rainbow-auth-firebase-auth_container', className);
 
     return (
-        <div className={className} style={style}>
-            <FormattedMessage
-                id="authenticated.exp.welcome"
-                values={userName}
-                defaultMessage={`Welcome ${userName.name}!!`} />
-            <Button label={intl.formatMessage(translations.logoutButton)} onClick={logoutUser} />
-        </div>
+        <section className={getContainerClassNames()} style={style}>
+            <div className="rainbow-auth-firebase-auth_top-bar">
+                <img
+                    src="/assets/rainbow-logo.svg"
+                    alt="rainbow logo"
+                    className="rainbow-auth-firebase-signin_image" />
+                <div className="rainbow-auth-firebase_top-bar-content">
+                    <Select options={languageOptions} className="rainbow-auth-firebase_language-selector" />
+                    <AvatarMenu
+                        id="avatar-menu"
+                        src="/assets/user2.jpg"
+                        assistiveText="Tahimi Leon"
+                        menuAlignment="right"
+                        menuSize="small"
+                        avatarSize="large"
+                        title="Tahimi Leon">
+                        <li>
+                            <Avatar
+                                src="/assets/user2.jpg"
+                                assistiveText="Tahimi Leon"
+                                title="Tahimi Leon"
+                                size="medium" />
+                            <div className="rainbow-m-left_x-small">
+                                <p className="rainbow-font-size-text_medium rainbow-color_dark-1">Tahimi</p>
+                                <p className="rainbow-font-size-text_small rainbow-color_gray-3">janedoe@gmail.com</p>
+                            </div>
+                        </li>
+                        <MenuDivider variant="space" />
+                        <MenuItem
+                            label={<FormattedMessage id="authenticated.profile.edit" defaultMessage="Edit profile" />}
+                            icon={<PencilIcon />}
+                            iconPosition="left" />
+                        <MenuItem
+                            label={<FormattedMessage id="authenticated.exp.logout" defaultMessage="Logout" />}
+                            onClick={logoutUser}
+                            icon={<PowerIcon />}
+                            iconPosition="left" />
+                    </AvatarMenu>
+                </div>
+            </div>
+            <span className="rainbow-auth-firebase-auth_title">Wellcome Tahimi</span>
+        </section>
     );
 }
 
@@ -49,7 +87,6 @@ AuthApp.propTypes = {
     style: PropTypes.object,
     user: PropTypes.object,
     logoutUser: PropTypes.func,
-    intl: intlShape.isRequired,
 };
 
 AuthApp.defaultProps = {
@@ -72,4 +109,4 @@ function dispatchToProps(dispatch) {
     }, dispatch);
 }
 
-export default connect(stateToProps, dispatchToProps)(injectIntl(AuthApp));
+export default connect(stateToProps, dispatchToProps)(AuthApp);

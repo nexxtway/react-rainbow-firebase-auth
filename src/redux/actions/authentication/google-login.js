adad8a0db6096d1e1800f75c047b95e0aa1dc0d8
cmcartaya <cmcartaya1sm@gmail.com>
2018-11-11 03:32:03 -0500

    fix: fix some errors

--- src/redux/actions/authentication/google-login.js
+++ src/redux/actions/authentication/google-login.js
@@ -1,15 +1,13 @@
-import getCurrentLocale from '../../services/app/get-current-locale';
 import { signInWithGoogle } from '../../services/firebase';
 import showErrorMessage from '../app/show-error-message';
 
 export const AUTH_START_WITH_GOOGLE = 'AUTH_START_WITH_GOOGLE';
 export const AUTH_SUCCESS_WITH_GOOGLE = 'AUTH_SUCCESS_WITH_GOOGLE';
 
-export default function loginWithFacebook() {
-    return (dispatch, getState) => {
+export default function loginWithGoogle() {
+    return (dispatch) => {
         dispatch({ type: AUTH_START_WITH_GOOGLE });
-        const locale = getCurrentLocale(getState());
-        return signInWithGoogle(locale, ['public_profile', 'email'])
+        return signInWithGoogle()
             .then(() => {
                 dispatch({ type: AUTH_SUCCESS_WITH_GOOGLE });
             })

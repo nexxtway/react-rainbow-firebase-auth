dcb08421214aa4d4085777ce28cd2c1a3234941a
ayanes <alejandro.yanes94@gmail.com>
2018-11-11 18:09:18 -0500

    fix: changed update profile action

--- src/redux/actions/profile/update-profile.js
+++ src/redux/actions/profile/update-profile.js
@@ -1,6 +1,7 @@
 import updateUserProfile from '../../services/firebase/update-profile';
 import changeCurrentUserEmail from '../../services/firebase/change-current-user-email';
 import changeCurrentUserPassword from '../../services/firebase/change-current-user-password';
+import getCurrentUser from '../../services/firebase/get-current-user';
 import showErrorMessage from '../app/show-error-message';
 import updateUserData from '../authentication/update-user-data';
 
@@ -11,23 +12,32 @@ export default function updateProfile(profile) {
     return (dispatch) => {
         dispatch({ type: UPDATE_PROFILE_START });
         const { displayName, email, password } = profile;
+        const { displayName: currentDisplayName, email: currentEmail } = getCurrentUser();
+        const promises = [];
 
-        return updateUserProfile({ displayName })
-            .then(() => {
-                dispatch(updateUserData({ displayName }));
-                return changeCurrentUserEmail(email);
-            })
-            .then(() => {
-                dispatch(updateUserData({ email }));
-                if (password !== undefined) {
-                    return changeCurrentUserPassword(password);
-                }
-                return Promise.resolve();
-            })
-            .then(() => dispatch({ type: UPDATE_PROFILE_END }))
-            .catch((error) => {
+        if (displayName !== currentDisplayName) {
+            promises.push(updateUserProfile({ displayName }));
+        }
+        if (email !== currentEmail) {
+            promises.push(changeCurrentUserEmail(email));
+        }
+        if (password !== undefined) {
+            promises.push(changeCurrentUserPassword(password));
+        }
+
+        if (promises.length > 0) {
+            return Promise.all(promises).then(() => {
+                const userData = {
+                    displayName,
+                    email,
+                };
+                dispatch(updateUserData(userData));
+                dispatch({ type: UPDATE_PROFILE_END });
+            }).catch((error) => {
                 dispatch({ type: UPDATE_PROFILE_END });
                 dispatch(showErrorMessage(error));
             });
+        }
+        return undefined;
     };
 }

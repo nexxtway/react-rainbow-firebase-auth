da84f3ee66876dd1c412a497aeda0badcbea995d
TahimiLeonBravo <tahimileon@gmail.com>
2018-11-13 15:11:56 -0600

    fix: change responsive design in userProfile page

--- src/components/pages/auth/home/index.js
+++ src/components/pages/auth/home/index.js
@@ -4,24 +4,15 @@ import classnames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import AvatarMenu from 'react-rainbow-components/components/AvatarMenu';
-import Avatar from 'react-rainbow-components/components/Avatar';
-import MenuDivider from 'react-rainbow-components/components/MenuDivider';
-import MenuItem from 'react-rainbow-components/components/MenuItem';
-import { navigateTo } from '../../../../history';
-import { logoutUser } from '../../../../redux/actions/authentication';
-import LanguageSelector from '../../../experiences/language-selector';
-import PowerIcon from '../../../icons/power';
-import PencilIcon from '../../../icons/pencil';
-import PersonIcon from '../../../icons/person';
+import TopBar from '../top-bar/index.js';
 import './styles.css';
+import './media-queries.css';
 
 function Home(props) {
     const {
         className,
         style,
         user,
-        logoutUser,
     } = props;
 
     const userName = {
@@ -32,48 +23,7 @@ function Home(props) {
 
     return (
         <section className={getContainerClassNames()} style={style}>
-            <div className="rainbow-auth-firebase-auth_top-bar">
-                <img
-                    src="/assets/rainbow-logo.svg"
-                    alt="rainbow logo"
-                    className="rainbow-auth-firebase-signin_image" />
-                <div className="rainbow-auth-firebase_top-bar-content">
-                    <LanguageSelector />
-                    <AvatarMenu
-                        src={user.photoURL}
-                        icon={<PersonIcon />}
-                        assistiveText={user.displayName}
-                        menuAlignment="right"
-                        menuSize="small"
-                        avatarSize="large"
-                        className="rainbow-auth-firebase_avatar-menu"
-                        title={user.displayName}>
-                        <li className="rainbow-auth-firebase_avatar-menu_user">
-                            <Avatar
-                                src={user.photoURL}
-                                icon={<PersonIcon />}
-                                assistiveText={user.displayName}
-                                title={user.displayName}
-                                size="medium" />
-                            <div className="rainbow-auth-firebase_avatar-menu_user-detail">
-                                <p className="rainbow-auth-firebase_avatar-menu_user-name">{user.displayName}</p>
-                                <p className="rainbow-auth-firebase_avatar-menu_user-email">{user.email}</p>
-                            </div>
-                        </li>
-                        <MenuDivider variant="space" />
-                        <MenuItem
-                            onClick={() => navigateTo('profile')}
-                            label={<FormattedMessage id="authenticated.profile.edit" defaultMessage="Edit profile" />}
-                            icon={<PencilIcon />}
-                            iconPosition="left" />
-                        <MenuItem
-                            label={<FormattedMessage id="authenticated.exp.logout" defaultMessage="Logout" />}
-                            onClick={logoutUser}
-                            icon={<PowerIcon />}
-                            iconPosition="left" />
-                    </AvatarMenu>
-                </div>
-            </div>
+            <TopBar />
             <span className="rainbow-auth-firebase-auth_title">
                 <FormattedMessage id="welcome" values={userName} defaultMessage={`Welcome ${user.displayName}`} />
             </span>
@@ -85,14 +35,12 @@ Home.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
     user: PropTypes.object,
-    logoutUser: PropTypes.func,
 };
 
 Home.defaultProps = {
     className: undefined,
     style: {},
     user: {},
-    logoutUser: () => {},
 };
 
 function stateToProps(state) {
@@ -104,7 +52,6 @@ function stateToProps(state) {
 
 function dispatchToProps(dispatch) {
     return bindActionCreators({
-        logoutUser,
     }, dispatch);
 }
 

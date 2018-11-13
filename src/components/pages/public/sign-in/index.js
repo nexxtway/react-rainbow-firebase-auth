f2bcd1fc94b4ffa38c7f95b2025b602881e0435d
TahimiLeonBravo <tahimileon@gmail.com>
2018-11-12 20:13:03 -0600

    feat: add terms and conditions to the login page

--- src/components/pages/public/sign-in/index.js
+++ src/components/pages/public/sign-in/index.js
@@ -103,6 +103,19 @@ class SignIn extends Component {
                             </Link>
                         </article>
                     </Card>
+                    <p className="rainbow-auth-firebase-signup_terms-conditions">
+                        <FormattedMessage id="sign.up.creating.account.agree" defaultMessage="By creating an account you agree to our" />
+                        <br />
+                        <Link className="rainbow-auth-firebase-signup_link" to="/home/terms">
+                            <FormattedMessage id="sign.up.terms" defaultMessage="Terms and Conditions" />
+                        </Link>
+                        <span>{' '}</span>
+                        <FormattedMessage id="sign.up.terms.and" defaultMessage="and our" />
+                        <span>{' '}</span>
+                        <Link className="rainbow-auth-firebase-signup_link" to="/home/privacy">
+                            <FormattedMessage id="sign.up.privacy" defaultMessage="Privacy Policy" />
+                        </Link>
+                    </p>
                 </section>
             </form>
         );

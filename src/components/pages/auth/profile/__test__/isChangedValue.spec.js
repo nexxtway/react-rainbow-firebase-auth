0fcf1feae1c4525262b78ce86f5b988dc70d8618
cmcartaya <cmcartaya1sm@gmail.com>
2018-11-15 15:38:01 -0500

    refactor: refactor the isChangedValue function and its test

--- src/components/pages/auth/profile/__test__/isChangedValue.spec.js
+++ src/components/pages/auth/profile/__test__/isChangedValue.spec.js
@@ -7,55 +7,50 @@ const initialValues = {
     password: undefined,
 };
 
-const anyValueChanged = {
-    displayName: 'React Firebase Authentication',
-    email: 'react.firebase.authentication@90milesbridge.com',
-    phoneNumber: undefined,
-    password: undefined,
-};
-
-const displayNameChanged = {
-    displayName: 'React Firebase',
-    email: 'react.firebase.authentication@90milesbridge.com',
-    phoneNumber: undefined,
-    password: undefined,
-};
-
-const emailChanged = {
-    displayName: 'React Firebase Authentication',
-    email: 'react.authentication@90milesbridge.com',
-    phoneNumber: undefined,
-    password: undefined,
-};
-
-const phoneNumberChanged = {
-    displayName: 'React Firebase Authentication',
-    email: 'react.firebase.authentication@90milesbridge.com',
-    phoneNumber: '12345678',
-    password: undefined,
-};
-
-const passwordChanged = {
-    displayName: 'React Firebase Authentication',
-    email: 'react.firebase.authentication@90milesbridge.com',
-    phoneNumber: undefined,
-    password: '90milesbridge',
-};
-
 describe('isChangedValue in <Profile/> page', () => {
     it('should return "false" when any values is changed', () => {
-        expect(isChangedValue(anyValueChanged, initialValues)).toBe(false);
+        const currentValues = {
+            displayName: 'React Firebase Authentication',
+            email: 'react.firebase.authentication@90milesbridge.com',
+            phoneNumber: undefined,
+            password: undefined,
+        };
+        expect(isChangedValue(currentValues, initialValues)).toBe(false);
     });
     it('should return "true" when the displayName value is changed', () => {
-        expect(isChangedValue(displayNameChanged, initialValues)).toBe(true);
+        const currentValues = {
+            displayName: 'React Firebase',
+            email: 'react.firebase.authentication@90milesbridge.com',
+            phoneNumber: undefined,
+            password: undefined,
+        };
+        expect(isChangedValue(currentValues, initialValues)).toBe(true);
     });
     it('should return "true" when the email value is changed', () => {
-        expect(isChangedValue(emailChanged, initialValues)).toBe(true);
+        const currentValues = {
+            displayName: 'React Firebase Authentication',
+            email: 'react.authentication@90milesbridge.com',
+            phoneNumber: undefined,
+            password: undefined,
+        };
+        expect(isChangedValue(currentValues, initialValues)).toBe(true);
     });
     it('should return "true" when the phoneNumber value is changed', () => {
-        expect(isChangedValue(phoneNumberChanged, initialValues)).toBe(true);
+        const currentValues = {
+            displayName: 'React Firebase Authentication',
+            email: 'react.firebase.authentication@90milesbridge.com',
+            phoneNumber: '12345678',
+            password: undefined,
+        };
+        expect(isChangedValue(currentValues, initialValues)).toBe(true);
     });
     it('should return "true" when the password value is changed', () => {
-        expect(isChangedValue(passwordChanged, initialValues)).toBe(true);
+        const currentValues = {
+            displayName: 'React Firebase Authentication',
+            email: 'react.firebase.authentication@90milesbridge.com',
+            phoneNumber: undefined,
+            password: '90milesbridge',
+        };
+        expect(isChangedValue(currentValues, initialValues)).toBe(true);
     });
 });
--- src/components/pages/auth/profile/isChangedValue.js
+++ src/components/pages/auth/profile/isChangedValue.js
@@ -1,12 +1,10 @@
-export default function isChangedValue(currentValues, initialsValues) {
-    if (currentValues.displayName !== initialsValues.displayName) {
-        return true;
-    } if (currentValues.email !== initialsValues.email) {
-        return true;
-    } if (currentValues.phoneNumber !== initialsValues.phoneNumber) {
-        return true;
-    } if (currentValues.password !== initialsValues.password) {
-        return true;
-    }
-    return false;
+const values = [
+    'displayName',
+    'email',
+    'phoneNumber',
+    'password',
+];
+
+export default function isChangedValue(currentValues, initialValues) {
+    return values.some(value => currentValues[value] !== initialValues[value]);
 }

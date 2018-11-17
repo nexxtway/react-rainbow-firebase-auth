c83ec40b824a3c2ff24a964220e3916ed3cecc48
LeandroTorresSicilia <jltorressicilia@gmail.com>
2018-11-17 02:23:53 -0500

    Update index.js

--- src/components/pages/auth/profile/index.js
+++ src/components/pages/auth/profile/index.js
@@ -105,7 +105,7 @@ function Profile(props) {
                                 placeholder={intl.formatMessage(translations.phonePlaceholder)}
                                 type="tel"
                                 icon={<PhoneIcon />} />
-                            <RenderIf isTrue={isSocialMediaUser}>
+                            <RenderIf isTrue={!isSocialMediaUser}>
                                 <Field
                                     component={Input}
                                     name="password"
